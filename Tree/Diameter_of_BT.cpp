2fddbe335f4dbab3b54ba4e73476a6ca77db469f
ARTI SHARMA <123803686+Arti52@users.noreply.github.com>
2023-07-03 23:59:39 +0530

    Create Diameter_of_BT.cpp

---
+++ Tree/Diameter_of_BT.cpp
@@ -0,0 +1,17 @@
+//Striver approach
+int height(TreeNode<int> *root,int &dia)
+{
+    if(root==NULL)return 0;
+    int lh=height(root->left,dia);
+    int rh=height(root->right,dia);
+    dia=max(dia,lh+rh);
+    return max(lh,rh)+1;
+
+}
+
+int diameterOfBinaryTree(TreeNode<int> *root)
+{
+	int dia=0;
+    height(root,dia);
+    return dia;
+}
