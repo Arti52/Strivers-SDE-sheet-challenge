2cd5d77008f90be9b1d8bfa4599e413a436b5236
ARTI SHARMA <123803686+Arti52@users.noreply.github.com>
2023-07-20 14:51:26 +0530

    Create median of matrix.cpp

---
+++ array/median of matrix.cpp
@@ -0,0 +1,30 @@
+//Time Complexity->O(n*mLog(n*m))+O(n*m) beats 23% space Complexity->O(n*m) beats 50%.
+#include<bits/stdc++.h>
+int getMedian(vector<vector<int>> &matrix)
+{
+    // Write your code here.
+    int n=matrix.size(),m=matrix[0].size();
+    vector<int> v(n*m);
+    int k=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            v[k]=matrix[i][j];
+            k++;
+        }
+    }
+    sort(v.begin(),v.end());
+    int mid;
+    if ((n * m) % 2 == 1) {
+      mid = ((n * m) + 1)/2;
+      return v[mid-1];
+    }
+    else
+    {
+         mid = (n * m)/2;
+         int ans=(v[mid-1]+v[mid])/2;
+         return ans;
+    }
+    return -1;
+}
